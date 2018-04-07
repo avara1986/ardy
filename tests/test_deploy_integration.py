c6d7ec66320bf3a4925221e67fb486a08c5ddaad
Alberto Vara <a.vara.1986@gmail.com>
2018-04-07 14:05:05 +0200

    Added new tests

--- tests/test_deploy_integration.py
+++ tests/test_deploy_integration.py
@@ -44,6 +44,62 @@ class DeployTest(DeployBaseTest):
     @patch.object(Build, "pip_install_to_target")
     @patch.object(Build, "copytree")
     @patch.object(Build, "create_artefact")
+    def test_run_with_version(self, create_artefact_mock, copytree_mock, pip_install_mock):
+        zip_file = MockZipFile.create_zip("test")
+        create_artefact_mock.return_value = zip_file
+
+        self.deploy = Deploy(path=os.path.dirname(os.path.abspath(__file__)), filename="config_with_alias_and_version.json")
+
+        # TODO: Search why moto rise errors
+        try:
+            # Create lambdas
+            self.deploy.run("myexamplelambdaproject")
+
+            self.assertTrue(pip_install_mock.called)
+            self.assertTrue(copytree_mock.called)
+            self.assertTrue(create_artefact_mock.called)
+
+            # Update lambdas
+            self.deploy.run("myexamplelambdaproject")
+
+        except ConnectionError as e:
+            print(e)
+
+        os.remove(zip_file)
+
+    @mock_s3
+    @mock_lambda
+    @patch.object(Build, "pip_install_to_target")
+    @patch.object(Build, "copytree")
+    @patch.object(Build, "create_artefact")
+    def test_run_with_version(self, create_artefact_mock, copytree_mock, pip_install_mock):
+        zip_file = MockZipFile.create_zip("test")
+        create_artefact_mock.return_value = zip_file
+
+        self.deploy = Deploy(path=os.path.dirname(os.path.abspath(__file__)), filename="config_with_version.json")
+
+        # TODO: Search why moto rise errors
+        try:
+            # Create lambdas
+            self.deploy.run("myexamplelambdaproject")
+
+            self.assertTrue(pip_install_mock.called)
+            self.assertTrue(copytree_mock.called)
+            self.assertTrue(create_artefact_mock.called)
+
+            # Update lambdas
+            self.deploy.run("myexamplelambdaproject")
+
+        except ConnectionError as e:
+            print(e)
+
+        os.remove(zip_file)
+
+    @mock_s3
+    @mock_lambda
+    @patch.object(Build, "pip_install_to_target")
+    @patch.object(Build, "copytree")
+    @patch.object(Build, "create_artefact")
     def test_run_with_alias(self, create_artefact_mock, copytree_mock, pip_install_mock):
         zip_file = MockZipFile.create_zip("test")
         create_artefact_mock.return_value = zip_file
